c143de09c0cdcfc8be7eef5464fd95f189be7414
Веселый человек <rustem.islamov.2016@gmail.com>
2023-11-12 17:07:00 +0300

    Добавьте файлы проекта.

---
+++ Data/Snils.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+using OfficeOpenXml;
+using System.IO.Packaging;
+
+namespace BlazorWebWorkASV.Data
+{
+    public class Snils
+    {
+        public string Number { get; set; }
+   
+    }
+}
---
+++ wwwroot/js/DownladFile.js
@@ -0,0 +1,34 @@
+﻿window.downloadFile = function (base64, fileName, contentType) {
+    const blob = base64toBlob(base64, contentType);
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+}
+
+function base64toBlob(base64, contentType) {
+    contentType = contentType || '';
+    const sliceSize = 1024;
+    const byteCharacters = atob(base64);
+    const bytesLength = byteCharacters.length;
+    const slicesCount = Math.ceil(bytesLength / sliceSize);
+    const byteArrays = new Array(slicesCount);
+
+    for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+        const begin = sliceIndex * sliceSize;
+        const end = Math.min(begin + sliceSize, bytesLength);
+
+        const bytes = new Array(end - begin);
+        for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+            bytes[i] = byteCharacters[offset].charCodeAt(0);
+        }
+
+        byteArrays[sliceIndex] = new Uint8Array(bytes);
+    }
+
+    return new Blob(byteArrays, { type: contentType });
+}
